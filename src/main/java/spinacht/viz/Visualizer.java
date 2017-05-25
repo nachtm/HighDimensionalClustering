f7ac1f7f645b81cd2db53d5f492288626a6790f0
nachtm <nachtm@carleton.edu>
2017-05-24 22:23:19 -0500

    Added options and buttons (not functional yet)

--- src/main/java/spinacht/viz/Visualizer.java
+++ src/main/java/spinacht/viz/Visualizer.java
@@ -1,16 +1,16 @@
 package spinacht.viz;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.awt.geom.Point2D;
@@ -29,8 +29,12 @@ public class Visualizer extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-//        BorderPane root = new BorderPane();
-        GridPane root = new GridPane();
+        List<Point2D.Double> points = new ArrayList<>();
+
+        FlowPane root = new FlowPane(Orientation.HORIZONTAL);
+
+        //build the canvases
+        GridPane canvases = new GridPane();
 
         StackPane canvasPane = new StackPane();
         StackPane xOnlyPane = new StackPane();
@@ -39,18 +43,56 @@ public class Visualizer extends Application {
         Canvas canv = new Canvas(500, 500);
         Canvas xOnly = new Canvas(500, 25);
         Canvas yOnly = new Canvas(25, 500);
-        Button btn = new Button("Save points");
 
         Pane[] panes = {canvasPane, xOnlyPane, yOnlyPane};
         Node[] nodes = {canv, xOnly, yOnly};
         addNodesAndStyle(panes, nodes, "bordered");
 
-        root.add(yOnlyPane, 0, 1);
-        root.add(xOnlyPane, 1, 0);
-        root.add(canvasPane, 1, 1);
-        root.add(btn, 1, 2);
+        canvases.add(yOnlyPane, 0, 1);
+        canvases.add(xOnlyPane, 1, 0);
+        canvases.add(canvasPane, 1, 1);
+        root.getChildren().add(canvases);
 
-        List<Point2D.Double> points = new ArrayList<>();
+        //build the controls
+        FlowPane controls = new FlowPane(Orientation.VERTICAL);
+        controls.setVgap(10);
+
+        FlowPane buttons = new FlowPane();
+        buttons.setHgap(5);
+        Button saveButton = new Button("Save points");
+        Button goCluster = new Button("Cluster this!");
+        buttons.getChildren().add(saveButton);
+        buttons.getChildren().add(goCluster);
+
+        FlowPane numPoints = new FlowPane();
+        numPoints.getChildren().add(new Label("NumPoints:"));
+        Spinner<Integer> ptSpinner = new Spinner<>(1, Integer.MAX_VALUE, 50, 5);
+        ptSpinner.setEditable(true);
+        numPoints.getChildren().add(ptSpinner);
+
+        FlowPane epsilonSetter = new FlowPane();
+        Slider slider = new Slider();
+        slider.setOrientation(Orientation.VERTICAL);
+        epsilonSetter.getChildren().add(slider);
+        epsilonSetter.getChildren().add(new Canvas(300, 300));
+
+        controls.getChildren().add(buttons);
+        controls.getChildren().add(numPoints);
+        controls.getChildren().add(epsilonSetter);
+        root.getChildren().add(controls);
+
+        saveButton.setOnMouseClicked((MouseEvent e) -> {
+            File f = new File(outpath);
+            try (FileWriter fw = new FileWriter(f)){
+                for(Point2D.Double p : points){
+                    fw.write(p.getX() + " " + p.getY() + "\n");
+                }
+            } catch (Exception err){
+                System.err.println(err);
+            }
+            System.out.println("Points saved!");
+        });
+
         canv.setOnMouseClicked(e -> {
             double x = e.getX();
             double y = e.getY();
@@ -65,19 +107,9 @@ public class Visualizer extends Application {
             System.out.println(x + " " + y);
         });
 
-        btn.setOnMouseClicked((MouseEvent e) -> {
-            File f = new File(outpath);
-            try (FileWriter fw = new FileWriter(f)){
-                for(Point2D.Double p : points){
-                    fw.write(p.getX() + " " + p.getY() + "\n");
-                }
-            } catch (Exception err){
-                System.err.println(err);
-            }
-            System.out.println("Points saved!");
-        });
 
-        Scene s = new Scene(root);
+
+        Scene s = new Scene(root, 1000, 600);
         s.getStylesheets().add("borders.css");
         primaryStage.setScene(s);
         primaryStage.show();
