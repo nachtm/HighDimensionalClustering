0fc27888e0a902caf9c847f873b9ea75e192876e
Nick Spinale <spinalen@carleton.edu>
2017-05-31 05:52:27 -0500

    Almost done commenting

--- src/main/java/spinacht/subclu/DumbSUBCLU.java
+++ src/main/java/spinacht/subclu/DumbSUBCLU.java
@@ -11,10 +11,18 @@ import java.util.LinkedList;
 import java.util.function.Consumer;
 
 /**
+ * Naive subspace clustering algorithm that has the same output as SUBCLU.
+ * Used for testing purposes.
  * Created by nachtm on 5/25/17.
  */
 public class DumbSUBCLU {
 
+    /**
+     * Cluster using DBSCAN in each subspace, just like SUBCLU, with the given SUBCLU parameters,
+     * by naively clustering in every possible subspace. O(2^n), where n is the number of dimensions.
+     * @param params
+     * @return
+     */
     public static InMemoryClustering go(Params params){
         Index index = new Index(params.getDatabase());
         DBSCANNER dbscanner = new PaperDBScanner(params.getEps(), params.getMinPts(), index);
@@ -26,6 +34,11 @@ public class DumbSUBCLU {
         return clustering;
     }
 
+    /**
+     * Feed a linked list corresponding to every possible subsequence of (0, 1, ..., ndims - 1) to the given function.
+     * @param ndims Exclusive ceiling
+     * @param consumer Function to receive each subsequence
+     */
     private static void forEachSubspace(int ndims, Consumer<LinkedList<Integer>> consumer) {
         LinkedList<Integer> xs = new LinkedList<>();
         forEachPrefix(ndims, xs, () -> {
@@ -35,6 +48,10 @@ public class DumbSUBCLU {
         });
     }
 
+    /**
+     * Helper for forEachSubspace that mutates the given list to contain each subsequence, invoking the given Runnable
+     * each time the state of the list changes.
+     */
     private static void forEachPrefix(int x, LinkedList<Integer> xs, Runnable go) {
         if (x == 0) {
             go.run();
--- src/main/java/spinacht/subclu/SUBCLU.java
+++ src/main/java/spinacht/subclu/SUBCLU.java
@@ -3,13 +3,26 @@ package spinacht.subclu;
 import spinacht.Params;
 import spinacht.data.Clustering;
 
-
+/**
+ * Provides the function that implements the SUBCLU algorithm.
+ */
 public class SUBCLU {
 
+    /**
+     * Cluster in each subspace with SUBCLU with the given parameters.
+     * @param params Parameters to SUBCLU
+     * @return A Clustering object that represents the result of the clustering
+     */
     public static Clustering go(Params params) {
         return go(params, false);
     }
 
+    /**
+     * Cluster in each subspace with SUBCLU with the given parameters, optionally reporting progress (for debugging).
+     * @param params Parameters to SUBCLU
+     * @param verbose Whether to write debugging output to stdout
+     * @return A Clustering object that represents the result of the clustering
+     */
     public static Clustering go(Params params, boolean verbose){
         Trie trie = new Trie(params);
         int k = 1;
